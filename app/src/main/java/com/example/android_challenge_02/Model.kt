02dd05ea638ab4ebd46abe1e6c0fb606f05c797a
Victor Hugo Souza Ferreira-VICTORH <victorh@motorola.com>
2022-02-14 14:58:31 -0300

    FrontEnd Prototype Ready

---
+++ app/src/main/java/com/example/android_challenge_02/Model.kt
@@ -0,0 +1,14 @@
+package com.example.android_challenge_02
+
+import androidx.lifecycle.ViewModel
+
+class Model : ViewModel() {
+
+    fun getNewPhrase(){
+        //gets new phrase
+    }
+
+    fun getAllFromApi(){
+        //gets all phrases from api
+    }
+}
